d4234ae054be9eb84f3e6d204dddb18039b4f1b8
Ramon Rodrigues <ramonrodsouza@gmail.com>
2023-03-25 12:33:13 -0300

    Promise

--- projeto_inicial/admin/service/cliente-service.js
+++ projeto_inicial/admin/service/cliente-service.js
@@ -20,17 +20,35 @@ const criaNovaLinha = (nome, email) => {
 
 const tabela = document.querySelector('[data-tabela]')
 
-const http = new XMLHttpRequest()    // Faz a Comunicação
-http.open('GET', 'http://localhost:3000/profile')   // abre a comunicação da aplicação com a API
-http.send()
+const listaClientes = () => {
 
-http.onload = () => {   // Indicando pro JS oque irá acontecer depois que ele enviar a requisição 
-                        //metodo "onload" ao carregar a página, vai executar alguma coisa.                    
-    const data = JSON.parse(http.response)   // transformando essa "response" que vem em texto em um objeto JavaScript
+    const promise = new Promise ((resolve, reject) => {
+            
+        const http = new XMLHttpRequest();   // Faz a Comunicação
+        http.open('GET', 'http://localhost:3000/profile');   // abre a comunicação da aplicação com a API
+
+        http.onload = () => {   // Indicando pro JS oque irá acontecer depois que ele enviar a requisição  metodo "onload" ao carregar a página, vai executar alguma coisa. 
+            if( http.status >= 400){
+                reject(JSON.parse(http.response));  // transformando essa "response" que vem em texto em um objeto JavaScript
+
+            }else {
+
+                resolve(JSON.parse(http.response));
+            }
+        }
+        http.send();
+
+    })
+    
+    return promise;
+}
+
+listaClientes()
+// Depois de executar a função listaCliente e pegar a promesa ele vai exibiti isso na tela.
+.then (data => {  // "então" 
 
     data.forEach(element => {
         tabela.appendChild(criaNovaLinha(element.nome, element.email))// colocando o elemento filho que  seria a tabela " TR"  dentro do elemento pai " data-tabela"
 
     })
-
-}
+})
